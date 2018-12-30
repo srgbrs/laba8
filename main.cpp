f6d35ea9c7db00da102c9da8fe81d4bd00fe24f6
srgbrs <43040660+srgbrs@users.noreply.github.com>
2018-12-30 18:31:57 +0200

    Update main.cpp
    @nechaido

--- main.cpp
+++ main.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, const char * argv[]) {
-    
-    int n,z=0,max=-1,s=0; cin>>n;
-    int a[n][n];
+void MatrixShow(int** a, int n);
+void MatrixCalc(int** a, int n);
 
-    for (int i=0; i<n; i++)
-        for (int j=0; j<n; j++)
-            a[i][j]=rand()%8+1;
+
+int main(int argc, const char * argv[]) {
+    int n; cin>>n;
     
+    int** a = new int*[n];
     
+    for (int i=0; i<n; i++) {
+        a[i] = new int[n];
+        for (int j=0; j<n; j++) {
+            a[i][j]=1+rand()%9;
+        }
+    }
+
+    MatrixShow(a, n);
+    MatrixCalc(a, n);
+    
+    for (int i=0; i<n; i++) delete[] a[i];
+    delete[] a;
+    
+    return 0;
+}
+
+void MatrixShow(int** a, int n)
+{
     for (int i=0; i<n; i++) {
         for (int j=0; j<n; j++)
             cout<<a[i][j]<<" ";
@@ -19,6 +36,11 @@ int main(int argc, const char * argv[]) {
     }
     
     cout<<endl;
+}
+
+void MatrixCalc(int** a, int n)
+{
+    int z=0,max=-1,s=0;
     
     for (int i=0; i<(n/2); i++)
     {
@@ -50,6 +72,4 @@ int main(int argc, const char * argv[]) {
     }
     
     cout<<endl; cout<<"max "<<max<<endl<<"sum "<<s<<endl;
-    
-    return 0;
 }
